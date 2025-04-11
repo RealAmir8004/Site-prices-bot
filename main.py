946c3c74b7ad607f5b161bcfc530490fe49aad7c
Amiryazdan <amiryazdangh.1384@gmail.com>
2025-04-11 08:07:14 +0330

    Big changes before making UI : functional/clean code  improvment

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from bs4 import BeautifulSoup
 from pathlib import Path
 import os  
 import csv 
-from typing import Literal # ?
+from json import loads
+from bs4.element import Tag    
 
 
 class Data :
@@ -95,149 +96,114 @@ def get_data_from_csv() -> list[Data]:
 # تعداد محصول ناصفر در سایت تبدیل شده به محصولات فعال در سی اس وی 
 #  اینکه محصولات فعال در سی اس وی = false
 
-
-def scrap_data (data : list[Data]):
-    # fix me : removed for (tst) 
-    # age khasti "for" ro ezafe koni "def func" ha ro biyar biron !!!!!!!!!!!!!!! 
-    # for d in data:
-    #     if d.active and d.number>0 : 
-
-    d = data[22] #tst 
-    d = data[13] #tst
-    d = data[23] #tst
-    d = data[8] #tst  id =18
-    d.active = True # fix me : after fixing (ایرادات)
-# "بلندگو پایونیر Pioneer TS-G1020F - اصلی"G
+def search_google (d : Data):
+    print("searching google for ", d.id, ":")
+    from googlesearch import search
+    import re
+    query = d.name
+    num_results = 10
+    targets = []
+    target_pattern = r"https://torob.com/"
+    with open("searchResult_ME.txt","w") as file:
+        for result in search(query , num_results):
+            file.write(f"\n{result}\n")
+            if re.match(target_pattern,result):
+                targets.append( result)
     
-    def search_google (d : Data):
-        print("searching google for ", d.id, ":")
-        from googlesearch import search
-        import re
-        query = d.name
-        num_results = 10
-        targets = []
-        target_pattern = r"https://torob.com/"
-        with open("searchResult_ME.txt","w") as file:
-            for result in search(query , num_results):
-                file.write(f"\n{result}\n")
-                if re.match(target_pattern,result):
-                    targets.append( result)
-        
-        print("prinring all search results (first url will be used) :")
-        print(targets)
-        return targets
+    print("prinring all search results (first url will be used) :")
+    print(targets)
+    return targets
 
-    def get_html(url : str ) -> requests.Response:
-        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
-        try:
-            response = requests.get(url=url , headers=headers)
-            response.raise_for_status()
-        except HTTPError as err:
-            print(f'HTTP error occurred: {err}')  
-        except Exception as err:
-            print(f'Other error occurred: {err}') 
-        else:
-            print('Success! no expection with url : ' ,response.url) 
-            return response
+def get_html(url : str ) -> requests.Response:
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
+    try:
+        response = requests.get(url=url , headers=headers)
+        response.raise_for_status()
+    except HTTPError as err:
+        print(f'HTTP error occurred: {err}')  
+    except Exception as err:
+        print(f'Other error occurred: {err}') 
+    else:
+        print('Success! no expection with url : ' ,response.url) 
+        return response
+
+
+class Site :
+    def __init__(self , shop_name  , price : int , badged : bool = False ) :
+        self.name = shop_name
+        self.price = price 
+        self.badged = badged
+
+    def __lt__(self, other):  # Define sorting rule (lower grade first)
+        return self.price < other.price
     
-
-    class Site :
-        def __init__(self , shop_name  , price : int):
-            self.name = shop_name
-            self.price = price 
-        def __lt__(self, other):  # Define sorting rule (lower grade first)
-            return self.price < other.price
+    def suggest_price(self):
+        if self.badged: # ckeck if guarantee_badge !!!!!!!!! mohem : in behtarin halate  bekhate algoritm buy_box torob
+            print("issss  badged", end=" ") #tst
+            mod = self.price % 10_000
+            if mod <= 4000: # baraye inke age masalan ::  mod==0  bod price faghat 1000 ta kam nashe (zaye mishe)
+                mod += (4000-mod)
+            new_price = self.price - (mod + 1000)  # fix me : we can upgrade to new versions later (price_reduce->from after-csv-faze1 (commit) )
         
-    # (info@) : rah haye mokhtalefi baraye sort bodan  list ha hast 
-        # 1 - if : product['is_adv']  delete konim -> list ha khodeshon sort shode mian biron (vali ye data hazf mishe) 
-        # 2 - sortedList
-    from sortedcontainers import SortedList
-    import json # tst 
-    from json import loads
-    def get_all_sites (soup : BeautifulSoup)-> tuple[list[Site],list[Site],list[Site]]: 
+        else : # is NOT badged 
+            print("is Not badged", end=" ") #tst
+            new_price = ( self.price *105 ) / 100 - 1000 
+            mod = new_price % 10_000
+            if mod < 9000 and  self.price>=200_000  :
+                new_price =  new_price - (mod + 1000)  # we can upgrade to ...
 
-        script_tag = soup.find("script", {"id": "__NEXT_DATA__"})
+        return Site(self.name , new_price , self.badged)    
         
-        json_data = loads(script_tag.string)  
+    
+# (info@) : rah haye mokhtalefi baraye sort bodan  list ha hast 
+    # 1 - if : product['is_adv']  delete konim -> list ha khodeshon sort shode mian biron (vali ye data hazf mishe) 
+    # 2 - sortedList
+from sortedcontainers import SortedList
+import json # tst 
+def get_all_sites (soup : BeautifulSoup)-> tuple[list[Site],list[Site],list[Site]]: 
 
-        with open("test_data.json", "w", encoding="utf-8") as j: #tst
-            json.dump(json_data, j, ensure_ascii=False, indent=4)
-        
-        products = json_data["props"]["pageProps"]["baseProduct"]["products_info"]["result"]
+    script_tag = soup.find("script", {"id": "__NEXT_DATA__"})
+    
+    json_data = loads(script_tag.string)  
 
-        # (info@) 
-        badged_sites = []
-        unbadged_sites = []
-        sites = []
-        with open("other_sites.txt" , "w" , encoding='utf-8')as f : #tst (not all of down)
+    with open("test_data.json", "w", encoding="utf-8") as j: #tst
+        json.dump(json_data, j, ensure_ascii=False, indent=4)
+    
+    products = json_data["props"]["pageProps"]["baseProduct"]["products_info"]["result"]
 
-            for product in products:
-                if "اسپارک" in product['shop_name'] : 
-                    f.write(f"sparkkkk : {product['shop_name'] } ----------- {product['price_text']} \n") # tst
-                
-                elif product['availability'] and product['is_adv'] == False :
-                    if product['is_filtered_by_warranty'] :
-                        badged_sites.append(Site(product['shop_name']  , int(''.join(filter(str.isdigit, product['price_text']))) ))  
-                    else:
-                        unbadged_sites.append(Site(product['shop_name']  , int(''.join(filter(str.isdigit, product['price_text']))) ))
+    # (info@) 
+    badged_sites = []
+    unbadged_sites = []
+    sites : list[Site]= []
+    with open("other_sites.txt" , "w" , encoding='utf-8')as f : #tst (not all of down)
 
-                    sites.append(Site(product['shop_name']  , int(''.join(filter(str.isdigit, product['price_text']))) ))
+        for product in products:
+            shop = product['shop_name']
+            if "اسپارک" in shop : 
+                f.write(f"sparkkkk : {shop } ----------- {product['price_text']} \n") # tst
+                                
+            elif product['availability'] and product['is_adv'] == False :
 
-                    f.write(f"------{product['shop_name']} ----------- {product['price_text']}  \n") # tst
-                else :
-                    f.write(f"{product['shop_name'] } ----------- {product['price_text']} \n") # tst
-        return badged_sites , unbadged_sites, sites 
+                price = int(''.join(filter(str.isdigit, product['price_text'])))
 
-
-    def calc_price (soup : BeautifulSoup): 
-        from bs4.element import Tag        
-        
-        buy_box : Tag= soup.find('div', class_='Showcase_buy_box__q4cpD') # class = Showcase_buy_box__q4cpD # badge =.Showcase_guarantee_badge_text__Kz3AW
-        # code : age buy_box bdard nemikhord
-        #   buy_box = olaviat ydone  badish jaygozin she 
-
-        badged_sites , unbadged_sites ,  sites  = get_all_sites(soup) # sites  = all sites 
-        
-        box_name , box_price = buy_box.find_all('div' , class_='Showcase_buy_box_text__otYW_ Showcase_ellipsis__FxqVh')  
-        box = Site( box_name.get_text(strip=True)[8:] ,  int(''.join(filter(str.isdigit, box_price.get_text(strip=True)))) ) # ye object 'Site' az 'buy_box' misaze
-        print(f"buy box = {box.price}", end=" ") #tst
-        
-        if "اسپارک" in box.name: # need fix in new version : if ... and (buy_box.price == uncorrect ) 
-            
-            if not badged_sites:
-                box = unbadged_sites[0] # fix = code repetaion
-
-            elif badged_sites[0].price < (unbadged_sites[0].price * 105)/100 :
-                box = badged_sites[0]
-            else :
-                box = unbadged_sites[0] 
-            print(f"---> changed to : {box.price}", end=" ") #tst
-        
-
-        def calc_prices_pr (box : Site , badged_sites : list) :
-            if box.name in [site.name for site in badged_sites]: # ckeck if guarantee_badge !!!!!!!!! mohem : in behtarin halate  bekhate algoritm buy_box torob
-                print("issss  badged", end=" ")
-                mod = box.price % 10_000
-                if mod <= 4000: # baraye inke age masalan ::  mod==0  bod price faghat 1000 ta kam nashe (zaye mishe)
-                    mod += (4000-mod)
-                return   box.price - (mod + 1000)  # fix me : we can upgrade to new versions later (price_reduce->from after-csv-faze1 (commit) )
-            
-            else : # is NOT badged 
-                print("is Not badged", end=" ")
-                new_price = ( box.price *105 ) / 100 - 1000 
-                mod = new_price % 10_000
-                print (f" (tst : price={ box.price} <> mod={mod} ) " , end="")
-                if mod >= 9000 or  box.price<200_000  :
-                    return new_price
+                badged = product['is_filtered_by_warranty']
+                if badged :
+                    badged_sites.append(Site(shop  , price, True ))  
                 else:
-                    return  new_price - (mod + 1000)  
-                
-        
-        return calc_prices_pr(box , badged_sites)
+                    unbadged_sites.append(Site(shop  , price , False ))
+
+                sites.append(Site(shop  , price  , badged))
+
+                f.write(f"------{shop} ----------- {product['price_text']}  \n") # tst
+            else :
+                f.write(f"{shop } ----------- {product['price_text']} \n") # tst
+    return badged_sites , unbadged_sites, sites 
+
     
 
+def scrap (data : Data):
 
-    results = search_google(d)
+    results = search_google(data)
     print("searching torob :")
     response = get_html(results[0]) # targets[0] = first torob result(url)
 
@@ -246,21 +212,67 @@ def scrap_data (data : list[Data]):
     with  open("TorobResult3.html" , "w" , encoding='utf-8') as file  : #tst
         file.write(soup.prettify())
 
-    new_price = calc_price(soup)
+        #def get_sites (soup : BeautifulSoup):  
     
-    if new_price == False : # fix me (for tst ) : bayad "continue" kone bjaye "return" -> for loop
-        print("buy box was already spark")
-        return 
-    else:
-        print(f"Converted to :{new_price}")
-        return new_price
+    buy_box : Tag= soup.find('div', class_='Showcase_buy_box__q4cpD') # class = Showcase_buy_box__q4cpD # badge =.Showcase_guarantee_badge_text__Kz3AW
+    # code : age buy_box bdard nemikhord
+    #   buy_box = olaviat ydone  badish jaygozin she 
+
+    badged_sites , unbadged_sites ,  sites  = get_all_sites(soup) # sites  = all sites 
+    
+    box_name , box_price = buy_box.find_all('div' , class_='Showcase_buy_box_text__otYW_ Showcase_ellipsis__FxqVh')  
+    box = Site( box_name.get_text(strip=True)[8:] ,  int(''.join(filter(str.isdigit, box_price.get_text(strip=True)))) ) # ye object 'Site' az 'buy_box' misaze
+    if box.name in [site.name for site in badged_sites]: # ckeck if guarantee_badge !!!!!!!!! mohem : in behtarin halate  bekhate algoritm buy_box torob
+        box.badged = True
+    print(f"buy box = {box.price}", end=" ") #tst
+    
+    if "اسپارک" in box.name: # need fix in new version : if ... and (buy_box.price == uncorrect ) 
+        
+        if not badged_sites:
+            box = unbadged_sites[0] # fix = code repetaion
+
+        elif badged_sites[0].price < (unbadged_sites[0].price * 105)/100 :
+            box = badged_sites[0]
+        else :
+            box = unbadged_sites[0] 
+        print(f"---> changed to : {box.price}", end=" ") #tst
+    
+    return box , sites  
+    # return  badged_sites , unbadged_sites ,  sites , box  # age badged_sites , unbadged_sites  mikhaym 
+
+
+def UnUseAbale_show(box : Site , sites : list[Site] ): # fix me : delete me 
+    with open("suggestion.txt" , "w" , encoding='utf-8')as f: #tst
+        f.write("buy box : \n")
+        f.write(f"{box.price} calced with {box.name} \n")
+        f.write("suggestion : \n")
+        for i , site in enumerate(sites):
+            f.write(f"{i+1}- {site.price} calced with {site.name} \n")
+
+def ui():
+    pass # alan injayim !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
+
+def edit (data : Data, box : Site , sites : list[Site] ):
+    ui()
 
 
 # main.py :
+def main():
+    data = get_data_from_csv()
+    # for d in data:
+    #     loop_edit_data(d)
+    d = data[22] #tst 
+    d = data[13] #tst
+    d = data[23] #tst
+    d = data[8] #tst  id =18
+    d.active = True # fix me : after fixing (ایرادات)
 
-data = get_data_from_csv()
-scrap_data(data)
+    box , sites = scrap(d)
+    edit( d , box.suggest_price() , [site.suggest_price() for site in sites] )
+    
 
+
+main()
 
 # printed result :
 # suggestion : (if spark is first => start from row 2 ---> kolan spark ro har ja bod skip kon )
